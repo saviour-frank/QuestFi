f8bfde0ee0b5ce1ce31778bc501bb9af03690ad7
saviour-frank <saviourfrank979@gmail.com>
2026-05-17 16:37:20 +0100

    Fix duplicate imports and add conditional rendering for footer in ConditionalFooter component

--- frontend/components/layout/ConditionalFooter.tsx
+++ frontend/components/layout/ConditionalFooter.tsx
@@ -5,3 +5,8 @@ import Footer from './Footer'
 
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
+
+// Don't show footer on docs page
+  if (pathname?.startsWith('/docs')) {
+    return null
+  }
