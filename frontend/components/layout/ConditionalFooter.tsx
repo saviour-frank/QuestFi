7dda6c6f867e01fd2bc606b06e0f2099d4fc4896
saviour-frank <saviourfrank979@gmail.com>
2026-05-17 16:35:45 +0100

    Remove ConditionalFooter component and its dependencies

--- frontend/components/layout/ConditionalFooter.tsx
+++ frontend/components/layout/ConditionalFooter.tsx
@@ -1,15 +1,1 @@
 'use client'
-
-import { usePathname } from 'next/navigation'
-import Footer from './Footer'
-
-export default function ConditionalFooter() {
-  const pathname = usePathname()
-
-  // Don't show footer on docs page
-  if (pathname?.startsWith('/docs')) {
-    return null
-  }
-
-  return <Footer />
-}
