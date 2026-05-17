db872ba899f3cb02a69be777838eb15ca7ff0824
saviour-frank <saviourfrank979@gmail.com>
2026-05-17 16:37:34 +0100

    Fix duplicate imports and clean up ConditionalFooter component

--- frontend/components/layout/ConditionalFooter.tsx
+++ frontend/components/layout/ConditionalFooter.tsx
@@ -3,10 +3,13 @@
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
 
-import { usePathname } from 'next/navigation'
-import Footer from './Footer'
+export default function ConditionalFooter() {
+  const pathname = usePathname()
 
-// Don't show footer on docs page
+  // Don't show footer on docs page
   if (pathname?.startsWith('/docs')) {
     return null
   }
+
+  return <Footer />
+}
