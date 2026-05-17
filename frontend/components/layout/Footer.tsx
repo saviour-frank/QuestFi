ad7d92aadc34b361179b75312f18494fa28cf235
saviour-frank <saviourfrank979@gmail.com>
2026-05-17 16:39:32 +0100

    Add Scene3D function with floating 3D elements to Footer component

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -6,3 +6,8 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { Suspense, useRef } from 'react'
 import { Sphere, MeshDistortMaterial, Float } from '@react-three/drei'
 import * as THREE from 'three'
+
+// Floating 3D Elements
+function Scene3D() {
+  const sphere1 = useRef<THREE.Mesh>(null!)
+  const sphere2 = useRef<THREE.Mesh>(null!)
